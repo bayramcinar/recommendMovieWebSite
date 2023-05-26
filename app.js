139d1c1169d2309ad3372f2d628d239ef6de29b2
Bayram Çınar <99193151+bayramcinar@users.noreply.github.com>
2023-05-26 16:08:09 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+var card = document.getElementById("card");
+var changeButton = document.getElementById("changeButton");
+
+var jsonDosyaURL = 'newDosya3.json';
+var jsonVerisi = null;
+
+var istek = new XMLHttpRequest();
+istek.open('GET', jsonDosyaURL, true);
+istek.onreadystatechange = function() {
+  if (this.readyState === 4 && this.status === 200) {
+    jsonVerisi = JSON.parse(this.responseText);
+  }
+};
+istek.send();
+
+changeButton.addEventListener("click", function() { 
+  if (jsonVerisi) {
+    var randomIndex = Math.floor(Math.random() * jsonVerisi.length);
+    var randomMovie = jsonVerisi[randomIndex];
+
+    var cardTitle = card.querySelector(".card-title");
+    var cardText = card.querySelector(".card-text");
+    var timeElement = card.querySelector(".time span:last-child");
+    var releaseTimeElement = card.querySelector(".releaseTime span:last-child");
+    var imdbElement = card.querySelector(".imdb span:last-child");
+    var cardImage = card.querySelector(".card-img-top");
+    var video = card.querySelector(".video");
+
+    cardImage.src = randomMovie.img;
+    cardTitle.innerHTML = randomMovie.Name;
+    cardText.innerHTML = randomMovie.Description;
+    timeElement.innerHTML = randomMovie.Time;
+    releaseTimeElement.innerHTML = randomMovie.ReleaseDate;
+    imdbElement.innerHTML = randomMovie.imdbb;
+    video.src = randomMovie.Trailer;
+  }
+});
